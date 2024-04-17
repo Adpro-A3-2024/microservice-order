ec4008359be58efd2efd410396666eb6d8b7d408
murifq <muhamad.rifqi510@gmail.com>
2024-04-17 16:47:53 +0700

    initial commit

---
+++ src/test/java/id/ac/ui/cs/youkosu/microserviceorder/MicroserviceOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.youkosu.microserviceorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
