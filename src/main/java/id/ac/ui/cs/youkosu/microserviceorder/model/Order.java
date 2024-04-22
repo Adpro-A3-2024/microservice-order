679b3b84053fd6894ef06ec452c481e930176ca2
murifq <muhamad.rifqi510@gmail.com>
2024-04-22 22:36:32 +0700

    [GREEN] Implement Order model and Product temporary model

---
+++ src/main/java/id/ac/ui/cs/youkosu/microserviceorder/model/Order.java
@@ -0,0 +1,46 @@
+package id.ac.ui.cs.youkosu.microserviceorder.model;
+
+import id.ac.ui.cs.youkosu.microserviceorder.tempModel.Product;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter @Setter
+public class Order {
+    private String orderId;
+    private List<Product> products;
+    private String status;
+    Order(String orderId, List<Product> products, String status){
+        this.orderId = orderId;
+        this.status = status;
+
+        if (products.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            this.products = products;
+        }
+    }
+
+    Order(String orderId, List<Product> products){
+        this.orderId = orderId;
+        this.products = products;
+        this.status = "UNVERIFIED";
+    }
+
+    void setStatusToVerified(){
+        this.status = "VERIFIED";
+    }
+
+    void setStatusToCancelled(){
+        this.status = "CANCELLED";
+    }
+
+    void setStatusToShipped(){
+        this.status = "SHIPPED";
+    }
+
+    void setStatusToCompleted(){
+        this.status = "COMPLETED";
+    }
+}
