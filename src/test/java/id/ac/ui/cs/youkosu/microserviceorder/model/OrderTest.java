46548276cd9565b623ca402547364c82f76ce27c
murifq <muhamad.rifqi510@gmail.com>
2024-04-22 22:33:08 +0700

    [RED] Add tests for Order model

---
+++ src/test/java/id/ac/ui/cs/youkosu/microserviceorder/model/OrderTest.java
@@ -0,0 +1,118 @@
+package id.ac.ui.cs.youkosu.microserviceorder.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import id.ac.ui.cs.youkosu.microserviceorder.tempModel.Product;
+import id.ac.ui.cs.youkosu.microserviceorder.model.Order;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest{
+    private List<Product> products;
+    @BeforeEach
+    void setUp(){
+        this.products = new ArrayList<>();
+        Product product1 = new Product();
+        product1.setProductReqId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        product1.setProductReqName("Sampo Cap Bambang");
+        product1.setProductReqPrice(2);
+        product1.setProductReqPictureUrl("https://images.tokopedia.net/img/cache/900/hDjmkQ/2024/1/18/73592f9a-ef15-417f-8c6f-fd79c2f4228d.jpg");
+        product1.setProductReqSourceUrl("https://images.tokopedia.net/img/cache/900/hDjmkQ/2024/1/18/73592f9a-ef15-417f-8c6f-fd79c2f4228d.jpg");
+
+        Product product2 = new Product();
+        product2.setProductReqId("eb558e9f-1c39-460e-8860-71af6af63bd7");
+        product2.setProductReqName("Sabun Cap Usep");
+        product2.setProductReqPrice(3);
+        product2.setProductReqPictureUrl("https://images.tokopedia.net/img/cache/900/hDjmkQ/2024/1/18/73592f9a-ef15-417f-8c6f-fd79c2f4228d.jpg");
+        product2.setProductReqSourceUrl("https://images.tokopedia.net/img/cache/900/hDjmkQ/2024/1/18/73592f9a-ef15-417f-8c6f-fd79c2f4228d.jpg");
+        this.products.add(product1);
+        this.products.add(product2);
+    }
+
+    @Test
+    void testCreateOrderEmptyProduct() {
+        this.products.clear();
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            Order order = new Order("13652556-012a0-4c07-b546-54eb1396d79b",
+                    this.products, "UNVERIFIED");
+        });
+    }
+
+    @Test
+    void testCreateOrderDefaultStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products);
+
+        assertSame(this.products, order.getProducts());
+        assertEquals(2, order.getProducts().size());
+        assertEquals("Sampo Cap Bambang", order.getProducts().get(0).getProductReqName());
+        assertEquals("Sabun Cap Usep", order.getProducts().get(1).getProductReqName());
+
+        assertEquals("13652556-012a-4c07-b546-54eb1396d79b", order.getOrderId());
+        assertEquals("UNVERIFIED", order.getStatus());
+    }
+
+    @Test
+    void testCreateOrderVerifiedStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, "VERIFIED");
+        assertEquals("VERIFIED", order.getStatus());
+    }
+
+    @Test
+    void testCreateOrderCanceledStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, "CANCELED");
+        assertEquals("CANCELED", order.getStatus());
+    }
+
+    @Test
+    void testCreateOrderShippedStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, "SHIPPED");
+        assertEquals("SHIPPED", order.getStatus());
+    }
+
+    @Test
+    void testCreateOrderCompletedStatus() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, "COMPLETED");
+        assertEquals("COMPLETED", order.getStatus());
+    }
+
+    @Test
+    void testSetStatusToVerified() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, "UNVERIFIED");
+        order.setStatusToVerified();
+        assertEquals("VERIFIED", order.getStatus());
+    }
+
+    @Test
+    void testSetStatusToCancelled() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, "VERIFIED");
+        order.setStatusToCancelled();
+        assertEquals("CANCELLED", order.getStatus());
+    }
+
+    @Test
+    void testSetStatusToShipped() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, "VERIFIED");
+        order.setStatusToShipped();
+        assertEquals("SHIPPED", order.getStatus());
+    }
+
+    @Test
+    void testSetStatusToCompleted() {
+        Order order = new Order("13652556-012a-4c07-b546-54eb1396d79b",
+                this.products, "SHIPPED");
+        order.setStatusToCompleted();
+        assertEquals("COMPLETED", order.getStatus());
+    }
+
+}
