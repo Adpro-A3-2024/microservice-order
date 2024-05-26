c5aabcced71e287728779d48e054d01806f00f40
murifq <muhamad.rifqi510@gmail.com>
2024-05-26 16:48:03 +0700

    Fix ci-cd error

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
 	jacoco
 	id("org.sonarqube") version "4.4.1.3373"
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
 }
+
+group = "id.ac.ui.cs.youkosu"
+version = "0.0.1-SNAPSHOT"
 
 sonar {
 	properties {
@@ -14,8 +17,6 @@ sonar {
 	}
 }
 
-group = "id.ac.ui.cs.youkosu"
-version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_21
